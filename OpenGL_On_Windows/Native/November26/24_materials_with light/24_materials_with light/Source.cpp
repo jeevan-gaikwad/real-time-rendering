b731a9fa7311f4d3fa88617f9f8b50fcce437596
Jeevan Gaikwad <jcgaikwad@gmail.com>
2017-11-29 19:54:59 +0530

    24 materials with light assignment

---
+++ OpenGL_On_Windows/Native/November26/24_materials_with light/24_materials_with light/Source.cpp
@@ -0,0 +1,403 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<gl\GL.h>
+#include<gl\glu.h>
+#include"Material_prop.h"
+#pragma comment(lib,"opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+#define MY_NAME_INITIAL "JCG"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+bool gbIsFullScreen = false;
+HWND ghwnd;
+bool gbActiveWindow = false;
+bool gbEscapeKeyPressed = false;
+HDC ghdc;
+HGLRC ghrc;
+GLfloat angle_sphere = 0.0f;
+/*Lights on 3D objects*/
+GLboolean isLightingOn = false;
+GLfloat eye_z = 0.1f;
+GLfloat light_model_ambient[] = { 0.2f,0.2f,0.2f,0.0f };
+GLfloat light_model_local_viewer[] = { 0.0f };
+//Light0 //Red
+GLfloat light0_ambient[] = { 0.0f,0.0f,0.0f,0.0f };
+GLfloat light0_diffuse[] = { 1.0f,1.0f,1.0f,0.0f };
+GLfloat light0_specular[] = { 1.0f,1.0f,1.0f,0.0f };
+GLfloat light0_position[] = { 0.0f,0.0f,0.0f,0.0f };
+GLfloat angle_light = 0.0f;
+char current_axis_rotation = 'x';
+//Material
+GLfloat material_ambient[] = { 0.0f,0.0f,0.0f,0.0f };
+GLfloat material_diffuse[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat material_specular[] = { 1.0f,1.0f,1.0f,1.0f };
+GLfloat material_shininess = 50.0f;
+
+GLboolean isPyramidOn = GL_TRUE;
+GLboolean isCubeOn = GL_FALSE;
+GLboolean isSphereOn = GL_FALSE;
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	void initialize();
+	void uninitialize();
+	void update();
+	void display();
+	int iScreenWidth, iScreenHeight;
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("Light on 24 material spheres ");
+	bool bDone = false;
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+
+	if (!RegisterClassEx(&wndclass)) {
+		MessageBox(NULL, TEXT("Failed to register wndclass. Exiting"), TEXT("Error"), MB_OK);
+		exit(EXIT_FAILURE);
+	}
+	iScreenWidth = GetSystemMetrics(0);
+	iScreenHeight = GetSystemMetrics(1);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW, szAppName,
+		szAppName,
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		((iScreenWidth / 2) - 400), ((iScreenHeight / 2) - 300),
+		800, 600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ghwnd = hwnd;
+	initialize();
+	ShowWindow(hwnd, nCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	while (bDone == false) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				bDone = true;
+			}
+			else {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else {
+			update();
+			display();
+			if (gbActiveWindow == true) {
+				if (gbEscapeKeyPressed == true) {
+					bDone = true;
+				}
+			}
+		}
+	}
+	uninitialize();
+	return((int)msg.wParam);
+}
+void initialize() {
+	void resize(int, int);
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 8;
+	pfd.cDepthBits = 32;
+	ghdc = GetDC(ghwnd);
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE) {
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, light_model_ambient);
+	glLightModelfv(GL_LIGHT_MODEL_LOCAL_VIEWER, light_model_local_viewer);
+
+	//light0 i.e Red light properties
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+	//glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glEnable(GL_LIGHT0); //turn ON light0
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, material_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, material_shininess);
+
+	resize(800, 600);
+}
+void resize(int width, int height) {
+	if (height == 0)
+		height = 1;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+}
+void set_material_properties(material_props_t material_properties) {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material_properties.ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, material_properties.diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, material_properties.specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, material_properties.shininess);
+}
+void draw_spheres(GLint no_of_spheres, material_props_t material_properties[],int no_of_materials) {
+
+	void drawSphere();
+	void set_material_properties(material_props_t);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	GLfloat y_trans = 3.3f;
+	GLfloat x_trans = -3.0f;
+	glTranslatef(x_trans, y_trans, -10.0f);
+	int i = 0, j = 0;
+	for (i = 1, j=0;i <=no_of_spheres;i++,j++) {
+		
+		GLfloat local_y_trans = 0.0f;
+		if (((i -1)% 4) == 0 && (i-1!=0)) {
+			local_y_trans = 1.3f;
+			glMatrixMode(GL_MODELVIEW);
+			glLoadIdentity();
+			y_trans -= local_y_trans;
+			glTranslatef(x_trans, y_trans, -10.0f);
+			//glTranslatef(1.3f, y_trans, 0.0f);
+		}
+		
+		glTranslatef(1.3f, 0.0f, 0.0f);
+		//set material properties
+		if( j <no_of_materials)
+			set_material_properties(material_properties[j]); //our i starts with 1
+		drawSphere();
+	}
+}
+
+void drawSphere() {
+	GLUquadric *quadric = NULL;
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	quadric = gluNewQuadric();
+	gluSphere(quadric, 0.50f, 50, 50);
+	gluDeleteQuadric(quadric);
+}
+void set_light_rotation(char axis_of_rotation) {
+	if (axis_of_rotation == 'x') {
+		glRotatef(angle_light, 1.0f, 0.0f, 0.0f);
+		light0_position[1] = angle_light;
+		light0_position[0]= light0_position[2] = 0.0f;
+
+	}
+	else if (axis_of_rotation == 'y') {
+		glRotatef(angle_light, 0.0f, 1.0f, 0.0f);
+		light0_position[0] = angle_light;
+		light0_position[1] = light0_position[2] = 0.0f;
+	}
+	else if (axis_of_rotation == 'z') {
+		glRotatef(angle_light, 0.0f, 0.0f, 1.0f);
+		light0_position[0] = angle_light;
+		light0_position[1] = light0_position[2] = 0.0f;
+	}
+	//set the light position
+	glLightfv(GL_LIGHT0, GL_POSITION, light0_position);
+}
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glPushMatrix();
+	gluLookAt(0.0f, 0.0f, eye_z, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+	//Global white light animation
+	glPushMatrix();
+	set_light_rotation(current_axis_rotation);
+	glPopMatrix();
+
+	//draw sphere
+	glPushMatrix();
+	draw_spheres(24,materials,no_of_materials);
+	glPopMatrix();
+
+	//final pop for initial push
+	glPopMatrix();
+	SwapBuffers(ghdc);
+}
+
+void uninitialize(void)
+{
+	//UNINITIALIZATION CODE
+	DWORD dwStyle;
+	WINDOWPLACEMENT wpPrev;
+	if (gbIsFullScreen == true)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(TRUE);
+
+	}
+
+	wglMakeCurrent(NULL, NULL);
+
+	wglDeleteContext(ghrc);
+	ghrc = NULL;
+
+	ReleaseDC(ghwnd, ghdc);
+	ghdc = NULL;
+
+	DestroyWindow(ghwnd);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	static HDC hdc;
+	DWORD dwStyle;
+	static WINDOWPLACEMENT wpPrev;
+	BOOL isWp;
+	HMONITOR hMonitor;
+	MONITORINFO monitorInfo;
+	BOOL isMonitorInfo;
+	switch (iMsg)
+	{
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0)
+			gbActiveWindow = true;
+		else
+			gbActiveWindow = false;
+		break;
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_CHAR:
+		switch (LOWORD(wParam)) {
+		case 'x':
+			current_axis_rotation = 'x';
+			break;
+		case 'y':
+			current_axis_rotation = 'y';
+			break;
+		case 'z':
+			current_axis_rotation = 'z';
+		default:
+			break;
+		}
+		break;
+	case WM_KEYDOWN:
+		switch (LOWORD(wParam)) {
+		case VK_ESCAPE:
+			gbEscapeKeyPressed = true;
+			break;
+
+		case 0x4c://l
+			if (isLightingOn) {
+				isLightingOn = false;
+				glDisable(GL_LIGHTING);
+			}
+			else {
+				isLightingOn = true;
+				glEnable(GL_LIGHTING);
+			}
+			break;
+
+		case 0x46:	//f
+			dwStyle = GetWindowLong(hwnd, GWL_STYLE);
+			if (gbIsFullScreen == false) {
+				if (dwStyle & WS_OVERLAPPEDWINDOW) {
+					wpPrev.length = sizeof(WINDOWPLACEMENT);
+					isWp = GetWindowPlacement(hwnd, &wpPrev);
+					hMonitor = MonitorFromWindow(hwnd, MONITORINFOF_PRIMARY);
+					monitorInfo.cbSize = sizeof(MONITORINFO);
+					isMonitorInfo = GetMonitorInfo(hMonitor, &monitorInfo);
+					if (isWp == TRUE && isMonitorInfo) {
+						SetWindowLong(hwnd, GWL_STYLE, dwStyle&~WS_OVERLAPPEDWINDOW);
+						SetWindowPos(hwnd, HWND_TOP,
+							monitorInfo.rcMonitor.left,
+							monitorInfo.rcMonitor.top,
+							monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left,
+							monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top,
+							SWP_NOZORDER | SWP_FRAMECHANGED);
+						ShowCursor(FALSE);
+						gbIsFullScreen = true;
+					}
+				}
+			}
+			else { //restore
+				SetWindowLong(hwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE);
+				SetWindowPlacement(hwnd, &wpPrev);
+				SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+				ShowCursor(TRUE);
+				gbIsFullScreen = false;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+void update() {
+
+	if (angle_light < 360.f) {
+		angle_light = angle_light + 0.1f;		
+	}
+	else
+		angle_light = 0.0f;
+	
+}
