9da1edb99b4809b4b42eb1f06bad9d6a52e66992
Jeevan Gaikwad <jcgaikwad@gmail.com>
2018-01-14 14:54:31 +0530

    GPU_extensions_assign

---
+++ OpenGL_On_Windows/Native/January7/GPU_Extenstions/GPU_Extenstions/Source.cpp
@@ -0,0 +1,279 @@
+#include<iostream>
+#include <windows.h>
+#include <stdio.h>
+#include<fstream>
+#include <stdlib.h>
+#include <gl\glew.h>
+#include<gl\GL.h>
+
+#pragma comment(lib,"glew32.lib")
+#pragma comment(lib,"user32.lib")
+#pragma comment(lib,"gdi32.lib")
+#pragma comment(lib,"opengl32.lib")
+
+#define MY_NAME_INITIAL "JCG"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+bool gbIsFullScreen = false;
+HWND ghwnd;
+HDC ghdc;
+HGLRC ghrc;
+bool gbActiveWindow = false;
+bool gbEscapeKeyPressed = false;
+std::ofstream g_gpu_extensions_file;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	void initialize();
+	void uninitialize();
+	void update();
+	void display();
+	int iScreenWidth, iScreenHeight;
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("Programmable Pipeline template ");
+	bool bDone = false;
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+
+	if (!RegisterClassEx(&wndclass)) {
+		MessageBox(NULL, TEXT("Failed to register wndclass. Exiting"), TEXT("Error"), MB_OK);
+		exit(EXIT_FAILURE);
+	}
+	iScreenWidth = GetSystemMetrics(0);
+	iScreenHeight = GetSystemMetrics(1);
+
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW, szAppName,
+		szAppName,
+		WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+		((iScreenWidth / 2) - 400), ((iScreenHeight / 2) - 300),
+		800, 600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+	ghwnd = hwnd;
+	initialize();
+	ShowWindow(hwnd, nCmdShow);
+	SetForegroundWindow(hwnd);
+	SetFocus(hwnd);
+
+	while (bDone == false) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			if (msg.message == WM_QUIT) {
+				bDone = true;
+			}
+			else {
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else {
+			display();
+			if (gbActiveWindow == true) {
+				if (gbEscapeKeyPressed == true) {
+					bDone = true;
+				}
+			}
+		}
+	}
+	uninitialize();
+	return((int)msg.wParam);
+}
+void initialize() {
+	void resize(int, int);
+	void uninitialize();
+	PIXELFORMATDESCRIPTOR pfd;
+	int iPixelFormatIndex;
+
+	ZeroMemory(&pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.cRedBits = 8;
+	pfd.cGreenBits = 8;
+	pfd.cBlueBits = 8;
+	pfd.cAlphaBits = 32;
+	pfd.cDepthBits = 32;
+	ghdc = GetDC(ghwnd);
+	iPixelFormatIndex = ChoosePixelFormat(ghdc, &pfd);
+	if (iPixelFormatIndex == 0) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	if (SetPixelFormat(ghdc, iPixelFormatIndex, &pfd) == FALSE) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+
+	ghrc = wglCreateContext(ghdc);
+	if (ghrc == NULL) {
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	if (wglMakeCurrent(ghdc, ghrc) == FALSE) {
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+	}
+	GLenum glew_error = glewInit();	//Turn ON all graphic card extension
+	if (glew_error != GLEW_OK) {
+		wglDeleteContext(ghrc);
+		ghrc = NULL;
+		ReleaseDC(ghwnd, ghdc);
+		ghdc = NULL;
+		uninitialize();
+	}
+	//Know your GPU extensions
+	GLint numberOfExtensions = 0;
+	g_gpu_extensions_file.open("GPU_extensions.txt", std::ios::out);
+	
+	if (!g_gpu_extensions_file.is_open()) {
+		std::cout << "Failed to open a new file" << std::endl;
+		uninitialize();
+	}
+
+	glGetIntegerv(GL_NUM_EXTENSIONS, &numberOfExtensions);
+	for (int i = 0;i < numberOfExtensions;i++) {
+		g_gpu_extensions_file << glGetStringi(GL_EXTENSIONS, i)<<std::endl;
+	}
+	glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+
+	resize(800, 600);
+}
+void resize(int width, int height) {
+	if (height == 0)
+		height = 1;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-50.0, 50.0, -50.0, 50.0, -50.0, 50.0);
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	SwapBuffers(ghdc);
+}
+
+void uninitialize(void)
+{
+	//UNINITIALIZATION CODE
+	DWORD dwStyle;
+	WINDOWPLACEMENT wpPrev;
+	if (gbIsFullScreen == true)
+	{
+		dwStyle = GetWindowLong(ghwnd, GWL_STYLE);
+		SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+		SetWindowPlacement(ghwnd, &wpPrev);
+		SetWindowPos(ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED);
+
+		ShowCursor(TRUE);
+
+	}
+
+	wglMakeCurrent(NULL, NULL);
+
+	wglDeleteContext(ghrc);
+	ghrc = NULL;
+
+	ReleaseDC(ghwnd, ghdc);
+	ghdc = NULL;
+	if (g_gpu_extensions_file.is_open()) {
+		g_gpu_extensions_file.close();
+	}
+	DestroyWindow(ghwnd);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	static HDC hdc;
+	DWORD dwStyle;
+	static WINDOWPLACEMENT wpPrev;
+	BOOL isWp;
+	HMONITOR hMonitor;
+	MONITORINFO monitorInfo;
+	BOOL isMonitorInfo;
+	switch (iMsg)
+	{
+	case WM_ACTIVATE:
+		if (HIWORD(wParam) == 0)
+			gbActiveWindow = true;
+		else
+			gbActiveWindow = false;
+		break;
+	case WM_SIZE:
+		resize(LOWORD(lParam), HIWORD(lParam));
+		break;
+	case WM_KEYDOWN:
+		switch (LOWORD(wParam)) {
+		case VK_ESCAPE:
+			gbEscapeKeyPressed = true;
+			break;
+
+		case 0x46:	//f
+			dwStyle = GetWindowLong(hwnd, GWL_STYLE);
+			if (gbIsFullScreen == false) {
+				if (dwStyle & WS_OVERLAPPEDWINDOW) {
+					wpPrev.length = sizeof(WINDOWPLACEMENT);
+					isWp = GetWindowPlacement(hwnd, &wpPrev);
+					hMonitor = MonitorFromWindow(hwnd, MONITORINFOF_PRIMARY);
+					monitorInfo.cbSize = sizeof(MONITORINFO);
+					isMonitorInfo = GetMonitorInfo(hMonitor, &monitorInfo);
+					if (isWp == TRUE && isMonitorInfo) {
+						SetWindowLong(hwnd, GWL_STYLE, dwStyle&~WS_OVERLAPPEDWINDOW);
+						SetWindowPos(hwnd, HWND_TOP,
+							monitorInfo.rcMonitor.left,
+							monitorInfo.rcMonitor.top,
+							monitorInfo.rcMonitor.right - monitorInfo.rcMonitor.left,
+							monitorInfo.rcMonitor.bottom - monitorInfo.rcMonitor.top,
+							SWP_NOZORDER | SWP_FRAMECHANGED);
+						ShowCursor(FALSE);
+						gbIsFullScreen = true;
+					}
+				}
+			}
+			else { //restore
+				SetWindowLong(hwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE);
+				SetWindowPlacement(hwnd, &wpPrev);
+				SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER);
+				ShowCursor(TRUE);
+				gbIsFullScreen = false;
+			}
+			break;
+
+		default:
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
