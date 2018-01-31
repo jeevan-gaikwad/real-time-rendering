2bc4e9a3a2b3e7266a43bac8b44594f5015d82a4
Jeevan Gaikwad <jcgaikwad@gmail.com>
2018-01-31 08:39:25 +0530

    04-PP-multicolor-triangle

---
+++ OpenGL_On_Android/January28/04-PP-Multicolor-Triangle/app/src/main/java/com/jeevan/triangle_color/GLESMacros.java
@@ -0,0 +1,9 @@
+package com.jeevan.triangle_color;
+
+public class GLESMacros
+{
+	public static final int JCG_ATTRIBUTE_VERTEX = 0;
+	public static final int JCG_ATTRIBUTE_COLOR = 1;
+	public static final int JCG_ATTRIBUTE_NORMAL = 2;
+	public static final int JCG_ATTRIBUTE_TEXTURE0 = 3;
+}
