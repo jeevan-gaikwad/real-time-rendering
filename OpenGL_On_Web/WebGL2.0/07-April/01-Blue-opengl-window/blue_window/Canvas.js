212edbad4cb35d2df549113c6313d7f21a3a09f6
Jeevan Gaikwad <jcgaikwad@gmail.com>
2018-04-08 10:50:14 +0530

    WebGL-bluewindow

---
+++ OpenGL_On_Web/WebGL2.0/07-April/01-Blue-opengl-window/blue_window/Canvas.js
@@ -0,0 +1,136 @@
+//global variables
+var canvas=null;
+var gl=null; //for webgl context
+var bFullscreen = false;
+var canvas_original_width;
+var canvas_original_height;
+
+//To start animation
+var requestAnimationFrame = window.requestAnimationFrame ||
+							window.webkitRequestAnimationFrame||
+							window.mozRequestAnimationFrame||
+							window.oRequestAnimationFrame||
+							window.msRequestAnimationFrame;
+
+//To stop animation
+var cancelAnimationFrage = 
+			window.cancelAnimationFrame||
+			window.webkitCancelRequestAnimationFrame||
+			window.webkitCancelAnimationFrame||
+			window.mozCancelRequestAnimationFrame||
+			window.mozCancelAnimationFrame||
+			window.oCancelRequestAnimationFrame||
+			window.oCancelAnimationFrame||
+			window.msCancelRequestAnimationFrame||
+			window.msCancelAnimationFrame;
+			
+//on body load function
+function main()
+{
+	//get canvas elementFromPoint
+	canvas = document.getElementById("AMC");
+	if(!canvas)
+		console.log("Obtaining canvas from main document failed\n");
+	else
+		console.log("Obtaining canvas from main document succeeded\n");
+	//print obtained canvas width and height on console
+	console.log("Canvas width:" + canvas.width +" height:" +canvas.height);
+	canvas_original_width = canvas.width;
+	canvas_original_height = canvas.height;
+	
+	
+	//register keyboard and mouse event with window class
+	window.addEventListener("keydown", keydown, false);
+	window.addEventListener("click", mouseDown, false);
+	window.addEventListener("resize", resize, false);
+	
+	init();
+	resize();
+	draw();
+}
+function init(){
+	//Get OpenGL context
+	gl=canvas.getContext("webgl2");
+	if(gl == null)
+		console.log("Obtaining 2D webgl2 failed\n");
+	else
+		console.log("Obtaining 2D webgl2 succeeded\n");
+	
+	gl.viewportWidth  = canvas.width;
+	gl.viewportHeight  = canvas.height;
+	
+	gl.clearColor(0.0,0.0,1.0,1.0);
+	
+}
+
+function resize()
+{
+	if(bFullscreen == true)
+	{
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;		
+	}else
+	{
+		canvas.width = canvas_original_width;
+		canvas.height = canvas_original_height;
+	}
+	gl.viewport(0,0,canvas.width,canvas.height);
+}
+function draw()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	
+	requestAnimationFrame(draw,canvas);
+}
+function toggleFullScreen()
+{
+	//code
+	var fullScreen_element = 
+		document.fullscreenElement||
+		document.webkitFullscreenElement||
+		document.mozFullScreenElement||
+		document.msFullscreenElement||
+		null;
+		
+	//if not full screen
+	if(fullScreen_element == null)
+	{
+		if(canvas.requestFullscreen)
+			canvas.requestFullscreen();
+		else if(canvas.mozRequestFullScreen)
+			canvas.mozRequestFullScreen();
+		else if(canvas.webkitRequestFullscreen)
+			canvas.webkitRequestFullscreen();
+		else if(canvas.msRequestFullscreen)
+			canvas.msRequestFullscreen();
+	}
+	else //restore from fullscreen
+	{
+			if(document.exitFullscreen)
+				document.exitFullscreen();
+			else if(document.mozCancelFullScreen)
+				document.mozCancelFullScreen();
+			else if(document.webkitExitFullscreen)
+				document.webkitExitFullscreen();
+			else if(document.msExitFullscreen)
+				document.msExitFullscreen();
+	}
+}
+
+function keydown(event)
+{
+	switch(event.keyCode)
+	{
+		case 70: //for 'F' or 'f'
+			toggleFullScreen();
+			
+			//repaint
+			drawText("Hello world!!!");
+			break;
+	}
+}
+
+function mouseDown()
+{
+	alert("Mouse is clicked");
+}
