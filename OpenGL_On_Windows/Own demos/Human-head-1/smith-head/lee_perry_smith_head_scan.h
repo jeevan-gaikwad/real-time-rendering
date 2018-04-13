e03e20881dcd08ce930ad25f64fdc848571092b2
Jeevan Gaikwad <jcgaikwad@gmail.com>
2018-04-13 07:46:14 +0530

    Own demo: human head-smith head-per-frag

---
+++ OpenGL_On_Windows/Own demos/Human-head-1/smith-head/lee_perry_smith_head_scan.h
@@ -0,0 +1,13 @@
+// This is a .h file for the model: lee_perry_smith_head_scan
+
+// Positions: 9079
+// Texels: 9356
+// Normals: 9080
+// Faces: 17680
+// Vertices: 53040
+
+extern const long int lee_perry_smith_head_scanVertices;
+extern const float lee_perry_smith_head_scanPositions[159120];
+extern const float lee_perry_smith_head_scanTexels[106080];
+extern const float lee_perry_smith_head_scanNormals[159120];
+
